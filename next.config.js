125f149dcd37479be629169224237130f1d5e610
dssd1001 <dssd1001@gmail.com>
2023-04-15 17:12:47 +0900

    setup Next project, integrate Tailwind's Protocol frontend template

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+// const withTM = require("next-transpile-modules")([
+//   "ethtokyoproject_client/node_modules/echarts/charts.js",
+// ]);
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+// module.exports = withTM(nextConfig);
+module.exports = nextConfig;
---
+++ src/pages/_app.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Head from "next/head";
+import { Router, useRouter } from "next/router";
+// import { MDXProvider } from '@mdx-js/react'
+
+import { Layout } from "@/components/Layout";
+// import * as mdxComponents from '@/components/mdx'
+import { useMobileNavigationStore } from "@/components/MobileNavigation";
+
+import "@/styles/tailwind.css";
+import "focus-visible";
+
+import { Web3AuthProvider } from "@/services/web3auth";
+
+function onRouteChange() {
+  useMobileNavigationStore.getState().close();
+}
+
+Router.events.on("routeChangeStart", onRouteChange);
+Router.events.on("hashChangeStart", onRouteChange);
+
+export default function App({ Component, pageProps }) {
+  let router = useRouter();
+
+  const [web3AuthNetwork, setWeb3AuthNetwork] = useState(process.env.NEXT_PUBLIC_WEB3AUTH_NETWORK);
+  const [chain, setChain] = useState("polygon");
+
+  return (
+    <>
+      <Head>
+        {router.pathname === "/" ? (
+          <title>Bitcoin Robo-Advisor</title>
+        ) : (
+          <title>{`${pageProps.title}`}</title>
+        )}
+        <meta name="description" content={pageProps.description} />
+      </Head>
+      {/* <MDXProvider components={mdxComponents}> */}
+      <Web3AuthProvider chain={chain} web3AuthNetwork={web3AuthNetwork}>
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+      </Web3AuthProvider>
+      {/* </MDXProvider> */}
+    </>
+  );
+}
